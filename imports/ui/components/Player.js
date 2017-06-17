6d126b019ecd1d80455430d8eaa55484af2d7cdc
Nick Warner <nickwarner@gmail.com>
2017-06-17 16:32:26 -0400

    Advance measure display over time

--- imports/ui/components/Player.js
+++ imports/ui/components/Player.js
@@ -6,11 +6,21 @@ export default class Player extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      bpm: 85,
+      beatsPerMeasure: 4,
+      currentMeasureId: 1
     }
   }
 
   componentDidMount () {
     // this.osmd = new window.OSMD('omsd')
+    setInterval(() => {
+      let currentMeasureId = this.state.currentMeasureId + 1
+      if (currentMeasureId === 5) {
+        currentMeasureId = 1
+      }
+      this.setState({currentMeasureId})
+    }, this.millisecondsPerMeasure())
   }
 
   componentDidUpdate () {
@@ -26,23 +36,46 @@ export default class Player extends Component {
     //   )
   }
 
+  bpmToMilliseconds (bpm) {
+    return (60 * 1000) / bpm
+  }
+
+  millisecondsPerMeasure () {
+    return this.bpmToMilliseconds(this.state.bpm) * this.state.beatsPerMeasure
+  }
+
+  renderMeasure (measureId, index) {
+    let manipulator = this.props.manipulators.find((manipulator) => {
+      return manipulator.measureId === measureId
+    }) || {}
+    return (
+      <div key={manipulator._id} className={'measureContainer'} style={{opacity: (4 - index) / 4}}>
+        <div className={'measureCount'}>
+          {measureId + 1}
+        </div>
+        <div className={'measureContent'}>
+          {!!manipulator.patternData &&
+            <span>{manipulator.patternData.pattern.toString()}</span>
+          }
+        </div>
+      </div>
+    )
+  }
+
   render () {
-    // this.props.manipulators
-    console.log(this.props.manipulators)
     return (
       <div className='playerContainer'>
         <h1>Player {this.props.match.params._id}</h1>
         <div id='omsd' />
-        {this.props.manipulators.sort((a, b) => a.measureId - b.measureId).map((manipulator) => {
-          return (
-            <li key={manipulator._id}>
-              {manipulator.measureId} - {manipulator._id}:
-              {!!manipulator.patternData &&
-                <span>{manipulator.patternData.pattern.toString()}</span>
-              }
-            </li>
-          )
-        })}
+        <div>
+          {[0, 1, 2, 3].map((i, index) => {
+            let measure = this.state.currentMeasureId + i
+            if (measure >= 4) {
+              measure = measure - 4
+            }
+            return this.renderMeasure(measure, index)
+          })}
+        </div>
       </div>
     )
   }
